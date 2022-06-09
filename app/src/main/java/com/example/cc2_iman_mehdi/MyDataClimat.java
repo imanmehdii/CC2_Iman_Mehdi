aead4db3496f511583d2b52c4125112badcf3a13
IMAN MEHDI <MEHDI.IMAN1@ofppt-edu.ma>
2022-06-09 14:12:15 +0100

    Commit2

---
+++ app/src/main/java/com/example/cc2_iman_mehdi/MyDataClimat.java
@@ -0,0 +1,45 @@
+package com.example.cc2_iman_mehdi;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDataClimat extends SQLiteOpenHelper {
+    public MyDataClimat(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    public static String DB_NAME = "Climat.db";
+    public static String TABLE_NAME = "CLIMAT";
+    public static String COL1 = "ID";
+    public static String COL2 = "VILLE";
+    public static String COL3 = "PAYS";
+    public static String COL4 = "TEMPERATURE";
+    public static String COL5 = "POURCNUAGE";
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE " + TABLE_NAME + " (" + COL1 + " integer primary key autoincrement," + COL2 + " text," + COL3 + " text," + COL4 + " integer," + COL5 + " integer)";
+        SQLiteDatabase db = null;
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public static long AddClimat(SQLiteDatabase sql, Climat e){
+        ContentValues c = new ContentValues();
+        c.put(COL2,p.getVille());
+        c.put(COL3,p.getPays());
+        c.put(COL4,p.getTemperature());
+        c.put(COL5,p.getPourcNuage());
+        return sql.insert(TABLE_NAME,null,c);
+    }
+}
